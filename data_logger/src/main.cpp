e2720305b31286cd0bdae52e09a121121d4527f8
Fatemeh Shafiei Ardestani <fatemeshafieeardestani@gmail.com>
2025-12-01 19:45:14 -0500

    feat: data logger app completed

--- data_logger/src/main.cpp
+++ data_logger/src/main.cpp
@@ -79,7 +79,93 @@ int main(){
 
     }
 
+    void* context = zmq_ctx_new();
+    void* pull_socket = zmq_socket(context, ZMQ_PULL);
+    int rc_pull = zmq_connect(pull_socket, "tcp://localhost:5556");
+    if(rc_pull != 0){
+        std::cerr << "Failed to connect to the ZMQ PULL socket: " << zmq_strerror(errno) << "\n";
+        zmq_close(pull_socket);
+        zmq_ctx_term(context);
+        sqlite3_finalize(insert_stmt);
+        sqlite3_close(db);
+        return 0; 
+    }
+    std::cout << "Connected the ZMQ PULL socket. " << "\n";
 
+    while(true){
+        zmq_msg_t metadata_msg;
+        zmq_msg_init (&metadata_msg);
+        int rc_meta = zmq_msg_recv(&metadata_msg, pull_socket, 0);
+        if(rc_meta == -1){
+            std::cerr << "[ERROR] zmq_msg_recv(meta) failed: "
+                  << zmq_strerror(errno) << "\n";
+            zmq_msg_close(&metadata_msg);
+            continue;
+        }
+        std::string metadata_str(
+            static_cast<char*> (zmq_msg_data(&metadata_msg)), 
+            zmq_msg_size(&metadata_msg));
+        zmq_msg_close(&metadata_msg);
+        nlohmann::json metadata;
+        try{
+            metadata = nlohmann::json::parse(metadata_str);
+        } catch (const std::exception& e){
+            std::cerr<< "[ERROR] Failed to parse metadata JSON: " << e.what() << "\n";
+            continue;
+        }
+        std::cout << "Received meta: " << metadata.dump() << "\n";
+        
+
+        zmq_msg_t img_msg;
+        zmq_msg_init(&img_msg);
+        int rc_img = zmq_msg_recv(&img_msg, pull_socket, 0);
+        if (rc_img == -1){
+            std::cerr << "[ERROR] zmq_msg_recv(image) failed: " << zmq_strerror(errno) << "\n";
+            zmq_msg_close(&img_msg);
+            continue;
+        }
+
+        std::vector<unsigned char> buf (
+            static_cast<unsigned char*>(zmq_msg_data(&img_msg)),
+            static_cast<unsigned char*>(zmq_msg_data(&img_msg)) + zmq_msg_size(&img_msg)
+        );
+        std::cout<< buf.size()<<"\n";
+        zmq_msg_close(&img_msg);
+        std::cout << "Received image buffer size: " << buf.size() << "\n";
+        int seq_number = metadata.value("seq_number", -1);
+        std::string name = metadata.value("image_name", std::string("unknown"));
+        int rows = metadata.value("rows", -1);
+        int cols = metadata.value("cols", -1);
+        int kp_count = metadata.value("keypoint_count", -1);
+
+        sqlite3_reset(insert_stmt);
+        sqlite3_clear_bindings(insert_stmt);
+        int idx = 1;
+
+        sqlite3_bind_int(insert_stmt, idx++, seq_number);
+        sqlite3_bind_text(insert_stmt, idx++,  name.c_str(), -1, SQLITE_TRANSIENT);
+        sqlite3_bind_int(insert_stmt,   idx++, rows);
+        sqlite3_bind_int(insert_stmt,   idx++, cols);
+        sqlite3_bind_int(insert_stmt,   idx++, kp_count);
+        sqlite3_bind_text(insert_stmt,  idx++, metadata_str.c_str(), -1, SQLITE_TRANSIENT);
+        sqlite3_bind_blob(insert_stmt,  idx++, buf.data(),
+                          static_cast<int>(buf.size()), SQLITE_TRANSIENT);
+
+        int step_rc = sqlite3_step(insert_stmt);
+        if (step_rc != SQLITE_DONE) {
+            std::cerr << "[ERROR] sqlite3_step failed: "
+                      << sqlite3_errmsg(db) << "\n";
+            continue;
+        }
+
+        std::cout << "Inserted frame seq=" << seq_number
+                  << " with " << kp_count
+                  << " keypoints into database.\n";
+    }
+
+
+    zmq_close(pull_socket);
+    zmq_ctx_term(context);
     sqlite3_finalize(insert_stmt);
     sqlite3_close(db); 
 
